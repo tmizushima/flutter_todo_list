311977978c44e5ad21aada42f9198a5043ea29e6
Renkon117 <randyok555@gmail.com>
2019-07-17 22:18:02 +0900

    complete version of UI

--- lib/main.dart
+++ lib/main.dart
@@ -42,47 +42,56 @@ class _InputFieldTasksState extends State<InputFieldTasks> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        TextField(
-          controller: eCtrl,
-          onSubmitted: (text) {
-            listItems.add(text);
-            eCtrl.clear();
-            setState(() {});
-          },
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white,
-            hintText: "Things to do",
-            contentPadding:
-                const EdgeInsets.only(left: 19.0, bottom: 15.0, top: 15.0),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white),
-              borderRadius: BorderRadius.circular(25.7),
-            ),
-          ),
-          autofocus: false,
-          autocorrect: true,
-        ),
-        RaisedButton(
-          shape: new RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(25.7)),
-          onPressed: () {},
-          child: new SizedBox(
-            height: 40.0,
-            width: 40.0,
-            child: new IconButton(
-              padding: new EdgeInsets.symmetric(vertical: 10.0),
-              color: Colors.blueAccent,
-              hoverColor: Colors.white,
-              icon: Icon(
-                IconData(57669, fontFamily: 'MaterialIcons'),
+        Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.7), color: Colors.white),
+          margin: EdgeInsets.all(5.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: TextField(
+                  controller: eCtrl,
+                  onSubmitted: (text) {
+                    listItems.add(text);
+                    eCtrl.clear();
+                    setState(() {});
+                  },
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Things to do",
+                    contentPadding: const EdgeInsets.only(
+                        left: 25.0, bottom: 15.0, top: 15.0),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(25.7),
+                    ),
+                  ),
+                  autofocus: false,
+                  autocorrect: true,
+                ),
               ),
-              onPressed: () {
-                listItems.add(eCtrl.text);
-                eCtrl.clear();
-                setState(() {});
-              },
-            ),
+              Container(
+                child: RaisedButton(
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(25.7)),
+                  onPressed: () {},
+                  color: Colors.blueAccent,
+                  child: new IconButton(
+                    padding: new EdgeInsets.symmetric(vertical: 10.0),
+                    color: Colors.white,
+                    hoverColor: Colors.white,
+                    icon: Icon(
+                      IconData(57669, fontFamily: 'MaterialIcons'),
+                    ),
+                    onPressed: () {
+                      listItems.add(eCtrl.text);
+                      eCtrl.clear();
+                      setState(() {});
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
         Expanded(
@@ -92,35 +101,40 @@ class _InputFieldTasksState extends State<InputFieldTasks> {
             itemBuilder: (BuildContext context, int index) {
               return Card(
                 child: ListTile(
-                  title: Text(listItems[index]),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      IconButton(
-                        icon: Icon(alreadySaved
-                            ? Icons.remove_circle_outline
-                            : Icons.remove_circle),
-                        onPressed: () {},
+                      Expanded(child: Text(listItems[index])),
+                      Container(
+                        width: 40,
+                        child: GestureDetector(
+                            child: Icon(
+                              Icons.remove_circle,
+                              color: Colors.redAccent,
+                            ),
+                            onTap: () {
+                              print("Removed");
+                            }),
                       ),
-                      IconButton(
-                        icon: Icon(alreadySaved
-                            ? Icons.check_box_outline_blank
-                            : Icons.check_box),
-                        onPressed: () {},
+                      Container(
+                        width: 30,
+                        child: GestureDetector(
+                            child: Icon(Icons.check_box_outline_blank),
+                            onTap: () {
+                              print("Checked");
+                            }),
                       ),
                     ],
                   ),
                   onTap: () {
-                    setState(() {
-                      if (alreadySaved) {}
-                    });
+                    setState(() {});
                   },
                 ),
               );
             },
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
